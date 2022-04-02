8c459e9d7dd8ac69417c9b199012fa0f1d6e1d97
mark-gl <26033115+mark-gl@users.noreply.github.com>
2022-04-02 02:16:07 +0100

    Mast teleop!!!

--- curiosity_mars_rover_control/scripts/teleop_keyboard_mast.py
+++ curiosity_mars_rover_control/scripts/teleop_keyboard_mast.py
@@ -7,24 +7,17 @@ import threading
 import roslib; roslib.load_manifest('teleop_twist_keyboard')
 import rospy
 
-from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64
 
 import sys, select, termios, tty
 
 msg = """
-Reading from the keyboard  and Publishing to Twist!
+Reading from the keyboard  and Publishing to Mast!
 ---------------------------
 Moving around:
    u    i    o
    j    k    l
    m    ,    .
-For Holonomic mode (strafing), hold down the shift key:
----------------------------
-   U    I    O
-   J    K    L
-   M    <    >
-t : up (+z)
-b : down (-z)
 anything else : stop
 q/z : increase/decrease max speeds by 10%
 w/x : increase/decrease only linear speed by 10%
@@ -41,16 +34,6 @@ moveBindings = {
         ',':(-1,0,0,0),
         '.':(-1,0,0,1),
         'm':(-1,0,0,-1),
-        'O':(1,-1,0,0),
-        'I':(1,0,0,0),
-        'J':(0,1,0,0),
-        'L':(0,-1,0,0),
-        'U':(1,1,0,0),
-        '<':(-1,0,0,0),
-        '>':(-1,-1,0,0),
-        'M':(-1,1,0,0),
-        't':(0,0,1,0),
-        'b':(0,0,-1,0),
     }
 
 speedBindings={
@@ -62,10 +45,14 @@ speedBindings={
         'c':(1,.9),
     }
 
+#/curiosity_mars_rover/mast_02_joint_position_controller/command
+#/curiosity_mars_rover/mast_cameras_joint_position_controller/command
+
 class PublishThread(threading.Thread):
     def __init__(self, rate):
         super(PublishThread, self).__init__()
-        self.publisher = rospy.Publisher('/curiosity_mars_rover/ackermann_drive_controller/cmd_vel', Twist, queue_size = 1)
+        self.publisher_mast02 = rospy.Publisher('/curiosity_mars_rover/mast_02_joint_position_controller/command', Float64, queue_size = 1)
+        self.publisher_mastcameras = rospy.Publisher('/curiosity_mars_rover/mast_cameras_joint_position_controller/command', Float64, queue_size = 1)
         self.x = 0.0
         self.y = 0.0
         self.z = 0.0
@@ -86,9 +73,9 @@ class PublishThread(threading.Thread):
 
     def wait_for_subscribers(self):
         i = 0
-        while not rospy.is_shutdown() and self.publisher.get_num_connections() == 0:
+        while not rospy.is_shutdown() and self.publisher_mast02.get_num_connections() == 0:
             if i == 4:
-                print("Waiting for subscriber to connect to {}".format(self.publisher.name))
+                print("Waiting for subscriber to connect to {}".format(self.publisher_mast02.name)) # TODO: aslo check mast_cameras
             rospy.sleep(0.5)
             i += 1
             i = i % 5
@@ -113,33 +100,31 @@ class PublishThread(threading.Thread):
         self.join()
 
     def run(self):
-        twist = Twist()
+        mast02_msg = Float64()
+        mastcameras_msg = Float64()
         while not self.done:
             self.condition.acquire()
             # Wait for a new message or timeout.
             self.condition.wait(self.timeout)
 
             # Copy state into twist message.
-            twist.linear.x = self.x * self.speed
-            twist.linear.y = self.y * self.speed
-            twist.linear.z = self.z * self.speed
-            twist.angular.x = 0
-            twist.angular.y = 0
-            twist.angular.z = self.th * self.turn
+            #twist.linear.x = 
+            #twist.linear.y = self.y * self.speed
+            #twist.linear.z = self.z * self.speed
+            mast02_msg.data = self.th * self.turn
+            mastcameras_msg.data = self.x * -self.speed
 
             self.condition.release()
 
             # Publish.
-            self.publisher.publish(twist)
+            self.publisher_mast02.publish(mast02_msg)
+            self.publisher_mastcameras.publish(mastcameras_msg)
 
         # Publish stop message when thread exits.
-        twist.linear.x = 0
-        twist.linear.y = 0
-        twist.linear.z = 0
-        twist.angular.x = 0
-        twist.angular.y = 0
-        twist.angular.z = 0
-        self.publisher.publish(twist)
+        mast02_msg.data = 0
+        mastcameras_msg.data = 0
+        self.publisher_mast02.publish(mast02_msg)
+        self.publisher_mastcameras.publish(mastcameras_msg)
 
 
 def getKey(key_timeout):
@@ -159,7 +144,7 @@ def vels(speed, turn):
 if __name__=="__main__":
     settings = termios.tcgetattr(sys.stdin)
 
-    rospy.init_node('teleop_twist_keyboard')
+    rospy.init_node('teleop_mast')
 
     speed = rospy.get_param("~speed", 0.5)
     turn = rospy.get_param("~turn", 1.0)
