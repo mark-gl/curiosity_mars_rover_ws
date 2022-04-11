5afede0a92ce83e683570b87958dbf11bcb240d9
mark-gl <26033115+mark-gl@users.noreply.github.com>
2022-04-11 18:20:38 +0100

    MarsViz Formatting

--- curiosity_mars_rover_viz/scripts/run_server.py
+++ curiosity_mars_rover_viz/scripts/run_server.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
-import http.server, ssl
+import http.server
+import ssl
 import functools
-#import webbrowser
 
 server_address = ('', 8080)
 ssl_key_file = "/etc/ssl/certs/localcerts/server1.example.com.key"
 ssl_certificate_file = "/etc/ssl/certs/localcerts/server1.example.com.pem"
 
-Handler = functools.partial(http.server.SimpleHTTPRequestHandler, directory='../.././')
+Handler = functools.partial(
+    http.server.SimpleHTTPRequestHandler, directory='../.././')
 
 httpd = http.server.HTTPServer(server_address, Handler)
 httpd.socket = ssl.wrap_socket(httpd.socket,
@@ -17,5 +18,3 @@ httpd.socket = ssl.wrap_socket(httpd.socket,
                                certfile=ssl_certificate_file,
                                ssl_version=ssl.PROTOCOL_TLS)
 httpd.serve_forever()
-    
-#webbrowser.open('https://127.0.0.1:8080', new=2)
