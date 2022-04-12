772fa939bae82436a498a601ca4dfddfd329ed7e
mark-gl <26033115+mark-gl@users.noreply.github.com>
2022-04-12 08:08:45 +0100

    moved it

--- curiosity_mars_rover_viz/web/js/side-controls.js
+++ curiosity_mars_rover_viz/web/js/side-controls.js
@@ -396,37 +396,3 @@ function panorama() {
   }
 }
 
-var gazeboWorld = new ROSLIB.Service({
-  ros: ros,
-  name: "/gazebo/get_world_properties",
-  serviceType: "gazebo_msgs/GetWorldProperties",
-});
-
-gazeboWorld.callService(new ROSLIB.Message({}), function (result) {
-  console.error(result.model_names);
-  switch (result.model_names[0]) {
-    case "world_flat":
-      document.getElementById("ground_plane").setAttribute("visible", true);
-      break;
-    case "world_mars_path":
-      document.getElementById("mars_path").setAttribute("visible", true);
-      break;
-    case "world_mars_photo":
-      document.getElementById("mars_photo").setAttribute("visible", true);
-      break;
-    case "world_mars_terrain":
-      console.error("Moment");
-      var mars_terrain = document.createElement("a-entity");
-      mars_terrain.setAttribute("gltf-model", "#terrain");
-      mars_terrain.setAttribute("position", "0 0 0");
-      mars_terrain.setAttribute("rotation", "0 0 0");
-      mars_terrain.setAttribute("scale", "1 1 1");
-      mars_terrain.setAttribute("shadow", "receive: false");
-      mars_terrain.setAttribute("class", "cantap");
-      document.getElementById("a-scene").appendChild(mars_terrain);
-      break;
-    default:
-      document.getElementById("ground_plane").setAttribute("visible", true);
-      break;
-  }
-});
