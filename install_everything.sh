d692f30b44d3992c64921c3eb32824b0aedeffa8
mark-gl <26033115+mark-gl@users.noreply.github.com>
2022-05-05 16:01:23 +0100

    Apt update

--- install_everything.sh
+++ install_everything.sh
@@ -2,6 +2,7 @@
 default_ws='curiosity_mars_rover_ws'
 CATKIN_WORKSPACE=${1:-$default_ws}
 
+sudo apt update
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt install curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
