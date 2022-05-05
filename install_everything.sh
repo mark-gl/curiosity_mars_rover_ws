69418bbad47a9cb84f6fc22fb9c3c346cb40a6ef
mark-gl <26033115+mark-gl@users.noreply.github.com>
2022-05-05 16:37:14 +0100

    Fix commands

--- install_everything.sh
+++ install_everything.sh
@@ -4,7 +4,7 @@ CATKIN_WORKSPACE=${1:-$default_ws}
 
 sudo apt update
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install curl
+sudo apt install -y curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
 sudo apt install -y python3 ros-noetic-desktop-full
